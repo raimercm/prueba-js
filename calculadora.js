061ec372d07a01aa522408f951435c8bf43eb986
raimercm <raimercm@gmail.com>
2022-02-08 20:14:06 -0500

    archivos calc

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+let sumar=require('./sumar');
+let restar=require('./restar');
+let multiplicar=require('./multiplicar');
+let dividir=require('./dividir');
+
+console.log(sumar(2,3));
+console.log(restar(2,3));
+console.log(multiplicar(2,3));
+console.log(dividir(2,3));
+//console.log("asdfsad");
